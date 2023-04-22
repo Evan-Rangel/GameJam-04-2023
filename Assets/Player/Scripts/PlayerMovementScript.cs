03470670f45fbcd22d8bfb95a31b47e1f80d86f7
Evan-Rangel <evan.rangel@uniat.edu.mx>
2023-04-22 15:55:02 -0600

    Salto

--- Assets/Player/Scripts/PlayerMovementScript.cs
+++ Assets/Player/Scripts/PlayerMovementScript.cs
@@ -5,11 +5,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovementScript : MonoBehaviour
 {
     [SerializeField] PlayerInput input;
+    [SerializeField] float jumpForce;
+    [SerializeField] float movementVelocity;
+    [SerializeField] float gravityScale = 5;
+    [SerializeField] float fallGravityScale = 15;
 
+    float jumpF;
     Vector2 movDir;
     Rigidbody2D rb;
-    bool isJumping;
+    bool isJumping=false;
+    bool canJump =true;
     float xDir;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,25 +25,38 @@ public class PlayerMovementScript : MonoBehaviour
     private void Update()
     {
         xDir = input.actions["Move"].ReadValue<Vector2>().x;
-        rb.velocity = new Vector2(xDir, 0);
-        if (input.actions["Jump"].WasPressedThisFrame())
+        rb.velocity = new Vector2(xDir*movementVelocity, rb.velocity.y);
+        //jumpForce=input.actions["Jump"].butt
+        if (input.actions["Jump"].IsPressed() && !isJumping && jumpF>0)
         {
-            Debug.Log("Salto");
-            rb.AddForce(Vector2.up*5);
-        }
-        
-        RaycastHit2D hit= Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity);
-        if (hit.collider!=null)
-        {
-            //Debug.Log("RayLog");
-        }
+            rb.velocity = new Vector2(rb.velocity.x, jumpF);
 
+            jumpF -= Time.deltaTime*jumpForce;
+            canJump = true;
+            //isJumping = true;
+            // rb.AddForce(Vector2.up * jumpF, ForceMode2D.Impulse);
+            Debug.Log("Salto");
+        }
+        else if(jumpF!=jumpForce)
+        {
+            jumpF = jumpForce;
+            isJumping = true;
+        }
+        if (rb.velocity.y>0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else
+        {
+            rb.gravityScale = fallGravityScale;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Ground"))
         {
+            isJumping = false;
             Debug.Log("Piso");
         }
     }
