25c7d91f5ea140b12521d7c37e1d2de4b15371df
ProReaps <angeldejesussandovalbrito@gmail.com>
2023-04-28 03:09:25 -0700

    Audios del jugador
    jugador y cañon

--- Assets/Player/Scripts/PlayerMovementScript.cs
+++ Assets/Player/Scripts/PlayerMovementScript.cs
@@ -24,6 +24,15 @@ public class PlayerMovementScript : MonoBehaviour
     float jumpT;
 
     bool canMove=true;
+    bool frstCheck = false;
+
+    public AudioSource Ruidos;
+    public AudioClip Jump_Start;
+    public AudioClip Hits_Ground;
+    public AudioClip Damage_Big;
+    public AudioClip Damage_Med;
+    public AudioClip Damage_Sm;
+
 
     public void SetCanMove(bool _canMove)
     {
@@ -61,11 +70,15 @@ public class PlayerMovementScript : MonoBehaviour
                 }
                 jumpT -= Time.deltaTime;
                 canJump = true;
-
             }
             else if (jumpT != jumpTime)
             {
+                if (!isJumping)
+                {
+                    Ruidos.PlayOneShot(Jump_Start);
+                }
                 isJumping = true;
+
             }
             if (rb.velocity.y>0)
             {
@@ -85,11 +98,36 @@ public class PlayerMovementScript : MonoBehaviour
             jumpF = jumpForce;
             jumpT = jumpTime;
             isJumping = false;
+
+            if(frstCheck)
+            {
+                Ruidos.PlayOneShot(Hits_Ground);
+            }
+            else
+            {
+                frstCheck = true;
+            }
+            
         }
     }
     //Funcion pa recibir daño llamala y ponle cuanto quieres que se baje
     void TakeDamage(int amount)
     {
+        //random para el sonido
+        int num = Random.Range(1, 4);
+        switch (num)
+        {
+            case 1:
+                Ruidos.PlayOneShot(Damage_Sm);
+                break;
+            case 2:
+                Ruidos.PlayOneShot(Damage_Med);
+                break;
+            case 3:
+                Ruidos.PlayOneShot(Damage_Big);
+                break;
+        }
+
         //Si el escudo esta activo no recibe daño y se apaga
         if (shield == true)
         {
