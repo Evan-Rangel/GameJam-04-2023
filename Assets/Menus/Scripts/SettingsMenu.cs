70547cb72f2ca29ea4dffafb9b0b3a36a7a1fe2d
ProReaps <angeldejesussandovalbrito@gmail.com>
2023-04-28 04:18:20 -0700

    Slider de volumen

--- Assets/Menus/Scripts/SettingsMenu.cs
+++ Assets/Menus/Scripts/SettingsMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-
+    public AudioMixer AM;
     public void SetVolume(float volume)
     {
+        AM.SetFloat("Volume", (Mathf.Log10(volume) * 20)+10);
         Debug.Log(volume);
-
     }
 }
