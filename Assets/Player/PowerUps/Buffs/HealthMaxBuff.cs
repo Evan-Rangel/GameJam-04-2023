16ae4861b3db5f3b5333063a5cdd0c2a167a0ed9
ProReaps <angeldejesussandovalbrito@gmail.com>
2023-04-28 09:24:14 -0700

    UI (corazones)

--- Assets/Player/PowerUps/Buffs/HealthMaxBuff.cs
+++ Assets/Player/PowerUps/Buffs/HealthMaxBuff.cs
@@ -7,9 +7,19 @@ public class HealthMaxBuff : PowerUpsEffects
 {
     //Cantidad que aumentara
     public int amount;
+    private int maxH;
+    private int currMaxH;
     public override void Apply(GameObject target)
     {
         //Buscamos el script de la variable que queremos modificar, tomamos la variable y sumamos o restamos dependiendo que queremos hacer (Esto para este tipo de items)
-        target.GetComponent<PlayerMovementScript>().maxHealth += amount;
+        maxH = target.GetComponent<PlayerMovementScript>().maxHealth;
+        currMaxH = target.GetComponent<PlayerMovementScript>().currentMaxHealth;
+
+        while (currMaxH+amount > maxH)
+        {
+            amount--;
+        }
+
+        target.GetComponent<PlayerMovementScript>().currentMaxHealth += amount;
     }
 }
